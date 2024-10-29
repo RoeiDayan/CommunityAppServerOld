7cd2b32e2b2d2bf35e6c10d4f04f6e3f1731d218
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:05:31 +0200

    Update Report.cs

--- CommunityAppServer/CommunityAppServer/DTO/Report.cs
+++ CommunityAppServer/CommunityAppServer/DTO/Report.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using CommunityAppServer.Models;
 
 namespace CommunityAppServer.DTO
 {
@@ -26,27 +27,54 @@ namespace CommunityAppServer.DTO
         [Column(TypeName = "datetime")]
         public DateTime? CreatedAt { get; set; }
 
-        [ForeignKey("ComId")]
-        [InverseProperty("Reports")]
-        public virtual Community? Com { get; set; }
+        //[ForeignKey("ComId")]
+        //[InverseProperty("Reports")]
+        //public virtual Community? Com { get; set; }
 
-        [ForeignKey("Priority")]
-        [InverseProperty("Reports")]
-        public virtual Priority? PriorityNavigation { get; set; }
+        //[ForeignKey("Priority")]
+        //[InverseProperty("Reports")]
+        //public virtual Priority? PriorityNavigation { get; set; }
 
-        [InverseProperty("Report")]
-        public virtual ICollection<ReportFile> ReportFiles { get; set; } = new List<ReportFile>();
+        //[InverseProperty("Report")]
+        //public virtual ICollection<ReportFile> ReportFiles { get; set; } = new List<ReportFile>();
 
-        [ForeignKey("Status")]
-        [InverseProperty("Reports")]
-        public virtual Status? StatusNavigation { get; set; }
+        //[ForeignKey("Status")]
+        //[InverseProperty("Reports")]
+        //public virtual Status? StatusNavigation { get; set; }
 
-        [ForeignKey("Type")]
-        [InverseProperty("Reports")]
-        public virtual Type? TypeNavigation { get; set; }
+        //[ForeignKey("Type")]
+        //[InverseProperty("Reports")]
+        //public virtual Type? TypeNavigation { get; set; }
 
-        [ForeignKey("UserId")]
-        [InverseProperty("Reports")]
-        public virtual Account? User { get; set; }
+        //[ForeignKey("UserId")]
+        //[InverseProperty("Reports")]
+        //public virtual Account? User { get; set; }
+
+        public Models.Report GetReport()
+        {
+            Models.Report rep = new Models.Report();
+            rep.ReportId = ReportId;
+            rep.UserId = UserId;
+            rep.ComId = ComId;
+            rep.Text = Text;
+            rep.Priority = Priority;
+            rep.Type = Type;
+            rep.Status = Status;
+            rep.IsAnon = IsAnon;
+            rep.CreatedAt = CreatedAt;
+            return rep;
+        }
+        public Report(Models.Report rep)
+        {
+            this.ReportId = rep.ReportId;
+            this.UserId = rep.UserId;
+            this.ComId = rep.ComId;
+            this.Text = rep.Text;
+            this.Priority = rep.Priority;
+            this.Type = rep.Type;
+            this.Status = rep.Status;
+            this.IsAnon = rep.IsAnon;
+            this.CreatedAt = rep.CreatedAt;
+        }
     }
 }
