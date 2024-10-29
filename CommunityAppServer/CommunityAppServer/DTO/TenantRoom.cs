7d3b357d7957e1b0b09dc946752131ad7282925c
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:06:48 +0200

    Update TenantRoom.cs

--- CommunityAppServer/CommunityAppServer/DTO/TenantRoom.cs
+++ CommunityAppServer/CommunityAppServer/DTO/TenantRoom.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommunityAppServer.DTO
@@ -20,12 +20,30 @@ namespace CommunityAppServer.DTO
         [Column(TypeName = "datetime")]
         public DateTime? SessionEnd { get; set; }
 
-        [ForeignKey("ComId")]
-        [InverseProperty("TenantRooms")]
-        public virtual Community Com { get; set; } = null!;
+        //[ForeignKey("ComId")]
+        //[InverseProperty("TenantRooms")]
+        //public virtual Community Com { get; set; } = null!;
 
-        [ForeignKey("KeyHolderId")]
-        [InverseProperty("TenantRooms")]
-        public virtual Account KeyHolder { get; set; } = null!;
+        //[ForeignKey("KeyHolderId")]
+        //[InverseProperty("TenantRooms")]
+        //public virtual Account KeyHolder { get; set; } = null!;
+        public Models.TenantRoom GetTenantRoom()
+        {
+            Models.TenantRoom gtr = new Models.TenantRoom();
+            gtr.ComId = ComId;
+            gtr.Status = Status;
+            gtr.KeyHolderId = KeyHolderId;
+            gtr.SessionStart = SessionStart;
+            gtr.SessionEnd = SessionEnd;
+            return gtr;
+        }
+        public TenantRoom(Models.TenantRoom gtr)
+        {
+            this.ComId = gtr.ComId;
+            this.Status = gtr.Status;
+            this.KeyHolderId= gtr.KeyHolderId;
+            this.SessionStart = gtr.SessionStart;
+            this.SessionEnd = gtr.SessionEnd;
+        }
     }
 }
