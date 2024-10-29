d169910ed3918631223233e5bc0cfd92e8c2ea20
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:04:11 +0200

    Update Member.cs

--- CommunityAppServer/CommunityAppServer/DTO/Member.cs
+++ CommunityAppServer/CommunityAppServer/DTO/Member.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommunityAppServer.DTO
@@ -26,12 +26,39 @@ namespace CommunityAppServer.DTO
 
         public bool? IsProvider { get; set; }
 
-        [ForeignKey("ComId")]
-        [InverseProperty("Members")]
-        public virtual Community Com { get; set; } = null!;
+        //[ForeignKey("ComId")]
+        //[InverseProperty("Members")]
+        //public virtual Community Com { get; set; } = null!;
 
-        [ForeignKey("UserId")]
-        [InverseProperty("Members")]
-        public virtual Account User { get; set; } = null!;
+        //[ForeignKey("UserId")]
+        //[InverseProperty("Members")]
+        //public virtual Account User { get; set; } = null!;
+
+        public Models.Member GetMember()
+        {
+            Models.Member mem = new Models.Member();
+            mem.ComId = ComId;
+            mem.UserId = UserId;
+            mem.Role = Role;
+            mem.Balance = Balance;
+            mem.UnitNum = UnitNum;
+            mem.IsLiable = IsLiable;
+            mem.IsResident = IsResident;
+            mem.IsManager = IsManager;
+            mem.IsProvider = IsProvider;
+            return mem;
+        }
+        public Member(Models.Member mem)
+        {
+            this.ComId = mem.ComId;
+            this.UserId = mem.UserId;
+            this.Role = mem.Role;
+            this.Balance = mem.Balance;
+            this.UnitNum = mem.UnitNum;
+            this.IsLiable = mem.IsLiable;
+            this.IsResident= mem.IsResident;
+            this.IsManager = mem.IsManager;
+            this.IsProvider = mem.IsProvider;
+        }
     }
 }
