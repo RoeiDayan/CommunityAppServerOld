04c7a28b4df41cdf5889f404bc09a7fe3fff0aa5
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:05:01 +0200

    Update Payment.cs

--- CommunityAppServer/CommunityAppServer/DTO/Payment.cs
+++ CommunityAppServer/CommunityAppServer/DTO/Payment.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using CommunityAppServer.Models;
 
 namespace CommunityAppServer.DTO
 {
@@ -22,12 +23,36 @@ namespace CommunityAppServer.DTO
 
         public DateOnly? PayUntil { get; set; }
 
-        [ForeignKey("ComId")]
-        [InverseProperty("Payments")]
-        public virtual Community? Com { get; set; }
+        //[ForeignKey("ComId")]
+        //[InverseProperty("Payments")]
+        //public virtual Community? Com { get; set; }
 
-        [ForeignKey("UserId")]
-        [InverseProperty("Payments")]
-        public virtual Account? User { get; set; }
+        //[ForeignKey("UserId")]
+        //[InverseProperty("Payments")]
+        //public virtual Account? User { get; set; }
+        public Models.Payment GetPayment()
+        {
+            Models.Payment pay = new Models.Payment();
+            pay.PaymentId = PaymentId;
+            pay.ComId = ComId;
+            pay.UserId = UserId;
+            pay.Amount = Amount;
+            pay.MarkedPayed = MarkedPayed;
+            pay.WasPayed = WasPayed;
+            pay.PayFrom = PayFrom;
+            pay.PayUntil = PayUntil;
+            return pay;
+        }
+        public Payment(Models.Payment pay)
+        {
+            this.PaymentId = pay.PaymentId;
+            this.ComId = pay.ComId;
+            this.UserId = pay.UserId;
+            this.Amount = pay.Amount;
+            this.MarkedPayed= pay.MarkedPayed;
+            this.WasPayed= pay.WasPayed;
+            this.PayFrom = pay.PayFrom;
+            this.PayUntil = pay.PayUntil;
+        }
     }
 }
