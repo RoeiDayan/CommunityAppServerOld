3e339ef75e16c7bc8c3abb508ce977221063bca2
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:03:57 +0200

    Update Community.cs

--- CommunityAppServer/CommunityAppServer/DTO/Community.cs
+++ CommunityAppServer/CommunityAppServer/DTO/Community.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
@@ -28,23 +28,45 @@ namespace CommunityAppServer.DTO
         [Column(TypeName = "datetime")]
         public DateTime? CreatedAt { get; set; }
 
-        [InverseProperty("Com")]
-        public virtual ICollection<Member> Members { get; set; } = new List<Member>();
+        //[InverseProperty("Com")]
+        //public virtual ICollection<Member> Members { get; set; } = new List<Member>();
 
-        [InverseProperty("Com")]
-        public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+        //[InverseProperty("Com")]
+        //public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
 
-        [InverseProperty("Com")]
-        public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+        //[InverseProperty("Com")]
+        //public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
 
-        [InverseProperty("Com")]
-        public virtual ICollection<RoomRequest> RoomRequests { get; set; } = new List<RoomRequest>();
+        //[InverseProperty("Com")]
+        //public virtual ICollection<RoomRequest> RoomRequests { get; set; } = new List<RoomRequest>();
 
-        [InverseProperty("Com")]
-        public virtual ICollection<TenantRoom> TenantRooms { get; set; } = new List<TenantRoom>();
+        //[InverseProperty("Com")]
+        //public virtual ICollection<TenantRoom> TenantRooms { get; set; } = new List<TenantRoom>();
 
-        [ForeignKey("ComId")]
-        [InverseProperty("Coms")]
-        public virtual ICollection<Notice> Notices { get; set; } = new List<Notice>();
+        //[ForeignKey("ComId")]
+        //[InverseProperty("Coms")]
+        //public virtual ICollection<Notice> Notices { get; set; } = new List<Notice>();
+
+
+        public Models.Community GetCommunity()
+        {
+            Models.Community com = new Models.Community();
+            com.ComId = ComId;
+            com.ComName = ComName;
+            com.Body = Body;
+            com.ComCode = ComCode;
+            com.Picture = Picture;
+            com.CreatedAt = CreatedAt;
+            return com;
+        }
+        public Community(Models.Community com)
+        {
+            this.ComId = com.ComId;
+            this.ComName = com.ComName;
+            this.Body = com.Body;
+            this.ComCode = com.ComCode;
+            this.Picture = com.Picture;
+            this.CreatedAt = com.CreatedAt;
+        }
     }
 }
