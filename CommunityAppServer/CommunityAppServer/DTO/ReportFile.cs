8520c758f27fd74ff10035e395698f120b98b8bd
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:05:46 +0200

    Update ReportFile.cs

--- CommunityAppServer/CommunityAppServer/DTO/ReportFile.cs
+++ CommunityAppServer/CommunityAppServer/DTO/ReportFile.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http.HttpResults;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CommunityAppServer.DTO
 {
@@ -15,8 +17,23 @@ namespace CommunityAppServer.DTO
         [StringLength(5)]
         public string? RepFileExt { get; set; }
 
-        [ForeignKey("ReportId")]
-        [InverseProperty("ReportFiles")]
-        public virtual Report Report { get; set; } = null!;
+        //[ForeignKey("ReportId")]
+        //[InverseProperty("ReportFiles")]
+        //public virtual Report Report { get; set; } = null!;
+
+        public Models.ReportFile GetReportFile()
+        {
+            Models.ReportFile repf = new Models.ReportFile();
+            repf.ReportId = ReportId;
+            repf.FileName = FileName;
+            repf.RepFileExt = RepFileExt;
+            return repf;
+        }
+        public ReportFile(Models.ReportFile repf)
+        {
+            this.ReportId = repf.ReportId;
+            this.FileName = repf.FileName;
+            this.RepFileExt = repf.RepFileExt;
+        }
     }
 }
