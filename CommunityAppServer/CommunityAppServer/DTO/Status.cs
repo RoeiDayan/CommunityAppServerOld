8b62336e3087d727f33e392a741eb6cfd213f63d
RoeiDayan <138358935+RoeiDayan@users.noreply.github.com>
2024-10-29 13:06:36 +0200

    Update Status.cs

--- CommunityAppServer/CommunityAppServer/DTO/Status.cs
+++ CommunityAppServer/CommunityAppServer/DTO/Status.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommunityAppServer.DTO
@@ -12,7 +12,20 @@ namespace CommunityAppServer.DTO
         [StringLength(10)]
         public string? Status1 { get; set; }
 
-        [InverseProperty("StatusNavigation")]
-        public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+        //[InverseProperty("StatusNavigation")]
+        //public virtual ICollection<Report> Reports { get; set; } = new List<Report>();
+
+        public Models.Status GetStatus()
+        {
+            Models.Status stat = new Models.Status();
+            stat.StatNum = StatNum;
+            stat.Status1 = Status1;
+            return stat;
+        }
+        public Status(Models.Status stat)
+        {
+            this.StatNum = stat.StatNum;
+            this.Status1 = stat.Status1;
+        }
     }
 }
